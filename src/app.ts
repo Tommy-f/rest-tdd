a67e9d94311d260f6ae45074452f77b6113d7f66
rutbergphilip <philiprutberg00@gmail.com>
2021-11-16 19:44:20 +0100

    db startup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('I am alive');
+import express from 'express';
+require('dotenv').config();
+import { DatabaseService } from './common/database.connect';
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on ${process.env.PORT}`);
+});
+
+(async () => {
+  await DatabaseService.getInstance().dbConnect();
+  console.log(DatabaseService.getCollection('users'));
+})();
