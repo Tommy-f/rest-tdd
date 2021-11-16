76a5980534a201af30dc6d24d1e4bd6196045fd2
rutbergphilip <philiprutberg00@gmail.com>
2021-11-16 23:21:17 +0100

    router endpoints

---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+const router = Router();
+
+// Product routes
+router.get('/api/products/');
+router.get('/api/products/:id');
+router.post('/api/products/');
+router.put('/api/products/:id');
+router.delete('/api/products/:id');
+
+// User routes
+router.get('/api/users/');
+router.get('/api/users/:id');
+router.post('/api/users/');
+router.delete('/api/users/:id');
+
+// Cart routes
+router.get('/api/carts/:userLogin');
+router.post('/api/carts/:userLogin/');
+router.put('/api/carts/:userLogin/:id');
+router.delete('/api/carts/:userLogin/:id');
+
+export default router;
