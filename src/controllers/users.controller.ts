7b17f7916ca2393dfab98528002ab044cbd0693d
Tommy <tommy.frykman@gmail.com>
2021-11-19 19:51:55 +0100

    refactoring and added services

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,6 +2,7 @@ import { HttpException } from '../errors/api.errors';
 import Users from '../models/users.model';
 import type { Request, Response, NextFunction } from 'express';
 import { Document } from 'mongoose';
+import { findUserById, createNewUser } from '../services/users.services'
 
 export const getAllUsers = async (
   req: Request,
@@ -15,7 +16,7 @@ export const getAllUsers = async (
     }
     return res.status(200).json(users);
   } catch (error) {
-    return res.status(error.status).json({ message: error.message });
+    next(error)
   }
 };
 
@@ -23,17 +24,13 @@ export const getUser = async (
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<Response> => {
+): Promise<void> => {
   try {
-    const user: Document | null = await Users.findOne({
-      login: req.params.login,
-    });
-    if (!user) {
-      throw new HttpException(404, 'User Not Found');
-    }
-    return res.status(200).json(user);
+    const userData = await findUserById(req.params.login);
+
+    res.status(200).json({ data: userData });
   } catch (error) {
-    return res.status(error.status).json({ message: error.message });
+    next(error);
   }
 };
 
@@ -41,15 +38,14 @@ export const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<Response> => {
+): Promise<void> => {
   try {
-    const newUser = await new Users({
-      name: req.body.name,
-      login: req.body.login,
-    }).save();
-    return res.status(201).json(newUser);
+    const newUser = req.body
+    await createNewUser(newUser);
+
+    res.status(201).json({ data: newUser, message: 'New user added!' });
   } catch (error) {
-    throw new HttpException(500, 'Could not create user');
+    next(error);
   }
 };
 
@@ -68,6 +64,6 @@ export const deleteUser = async (
     await user.delete();
     return res.status(200).json({ message: 'User Deleted' });
   } catch (error) {
-    return res.status(error.status).json({ message: error.message });
+    next(error)
   }
 };
---
+++ src/services/users.services.ts
@@ -0,0 +1,30 @@
+import { IUser } from '../interfaces/users.interface'
+import Users from '../models/users.model'
+import { HttpException } from '../errors/api.errors'
+import { isEmpty } from '../utils/utils'
+
+export const findUserById = async (login: string): Promise<IUser> => {
+
+  if (isEmpty(login)) throw new HttpException(400, "No id provided in the query");
+
+  const findUser: IUser = await Users.findOne({ login: login });
+  if (!findUser) throw new HttpException(404, "No user with that login");
+
+  return findUser;
+}
+
+
+export const createNewUser = async (userData: any): Promise<IUser> => {
+  if (isEmpty(userData)) throw new HttpException(400, "No user data provided in the query");
+
+  const findUser: IUser = await Users.findOne({ login: userData.login });
+  if (findUser) throw new HttpException(409, `User ${userData.login} already exists`);
+
+  const newUser = await new Users({
+    name: userData.name,
+    login: userData.login,
+  }).save();
+
+
+  return userData
+}
